92d6b5391f57537600f9c4675d7a116340b7d374
MrNelson <delatorrenelson127@gmail.com>
2026-02-11 09:07:36 +0800

    remove stackblitz files and update ignores.

--- src/App.tsx
+++ src/App.tsx
@@ -40,15 +40,13 @@ function App() {
         <div className="drawer drawer-end">
           <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
           <div className="drawer-content flex flex-col">            
-            <Navbar/>            
-            <button onClick={() => generatePareto()}>Generate Pareto</button>
+            <Navbar/>                        
             <Clocks />
           </div>
           <TimeZoneList />
         </div>
       </div>
     </TimeZoneContext.Provider>
-
   );
 }
 
