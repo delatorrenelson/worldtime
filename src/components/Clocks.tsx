b961c48ce764fffc55615ccedcc669f71db247eb
nelson <delatorrenelson127@gmail.com>
2026-02-16 03:22:50 +0800

    setup statemanagement.

--- src/components/Clocks.tsx
+++ src/components/Clocks.tsx
@@ -1,18 +1,18 @@
 import React, { useContext, useEffect } from "react";
 import Clock from "./Clock";
-import {  TimeZoneContext } from "../context/TimeZoneContext";
-import { uuid } from "../../utils/uuid";
+import { TimeZoneContext } from "../context/TimeZoneContext";
+import { uuid } from "../utils/uuid";
 import { getSelectedClocks, removeClock } from "../context/LocalStorage";
 
 export default function Clocks() {
   const txs = useContext(TimeZoneContext);
   txs.selectedTimeZones = getSelectedClocks()
-  const {selectedTimeZones, setSelectedTimeZones} = useContext(TimeZoneContext);    
+  const { selectedTimeZones } = useContext(TimeZoneContext);
 
   return (
     <div className="flex flex-wrap justify-center gap-12 py-6">
       {selectedTimeZones.map((clck) => (
-        <Clock key={uuid(5)} clock={clck}/>
+        <Clock key={uuid(5)} clock={clck} />
       ))}
     </div>
   );
